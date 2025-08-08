9c3f2a53b538f80f10ed3451d0c453c8fcac9d73
behradshahabi <behradshahabi4@gmail.com>
2025-08-08 03:28:21 -0700

    first commit

---
+++ tamrin_3/masahatdayere.py
@@ -0,0 +1,5 @@
+shoa = int (input ( "shoa e morede nazar ra vared konid :  "))
+adadp = float (3.14)
+formool = (shoa*shoa*adadp)
+
+print ( formool)
---
+++ tamrin_4/ahvalporsi.py
@@ -0,0 +1,2 @@
+name =input ("hello sir, please enter your name :  ")
+print  (f"hi {name} nice too meet you . how are you ?")
