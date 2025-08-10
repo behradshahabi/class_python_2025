1231a55e34ab938ea478ba65887088aba193dec6
behradshahabi <behradshahabi4@gmail.com>
2025-08-10 05:14:32 -0700

    tamrin 8

---
+++ tamrin_8/entekhbat.py
@@ -0,0 +1,8 @@
+sen = int(input("lotfan sen khod ra vared konid : "))
+
+if sen > 17:
+    print ("raye shoma sabt shaod")
+else:
+    print ("shoma hanooz be sene ghanooni naresidid ! ")
+
+print ("THE END")
