4b0bcb513d3004762d55eb6a3039496f0e915be8
behradshahabi <behradshahabi4@gmail.com>
2025-08-17 05:20:27 -0700

    tamrin 9 takmil shod

---
+++ tamrin_9/shahrebazi.py
@@ -0,0 +1,7 @@
+sen = int(input(" lotfan sen khod ra vared konid :  "))
+if sen < 12 :
+    print("gheimate belit : $5000")
+elif sen > 18 :
+    print ("gheimate belit : $20000")
+else :
+    print ("gheimate belit : $10000")
