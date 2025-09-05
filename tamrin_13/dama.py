9a39211deb4f8d22ef99d099d82ee335bab7000c
behradshahabi <behradshahabi4@gmail.com>
2025-09-05 00:01:04 -0700

    tamrin_13 takmil

---
+++ tamrin_13/dama.py
@@ -0,0 +1,10 @@
+damaha = [ 16, 24, 18, 49, 11, 1, 32, 25, 8]
+kamtarin = 100
+bishtarin = 0
+for d in damaha:
+    if d < kamtarin :
+        kamtarin = d
+    if d > bishtarin :
+        bishtarin = d
+print (f"kamtarin dama :  {kamtarin}")
+print (f"bishtarin dama :  {bishtarin}")
