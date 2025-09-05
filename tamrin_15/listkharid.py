9d7087f67f5a46f53d4372ef64a19741c833d9d1
behradshahabi <behradshahabi4@gmail.com>
2025-09-05 01:09:42 -0700

    tamrin_14 takmil

---
+++ tamrin_15/listkharid.py
@@ -0,0 +1,6 @@
+listkharid = ["shir", "shocolat", "cake", "abmive"]
+soal = (input("dar kharid kodam mahsool shak darid ?   "))
+if soal in listkharid :
+    print ("in mahsool dar list shoma ast")
+else :
+    print (" in mahsool dar sabad kharid shoma vojoood nadarad")
